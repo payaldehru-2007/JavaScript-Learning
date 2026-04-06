4c4aeaca42084936b6aa0644ce865febdcddaa5b
payaldehru-2007 <payaldehru@gmail.com>
2026-04-06 20:14:19 +0530

    Added Javascript Practice Files

---
+++ 2_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); // code readability should be high
+console.log("payal");
+
+let name ="payal"
+let age = 18
+let isloggedin = false
+
+
+
+
+// data types//
+// number => 2 to power 54
+// bignit
+// string => ""
+// boolean => true/false
+// null => standalone value means where we don't know the exact value like there is problem in server so it didnot predict 
+// temperature or give 0 bcz it is temp so it prefer null
+// undefined => like let state; we didnot assign exact value
+// symbol => unique
+
+
+// object//
+console.log(typeof "payal");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
